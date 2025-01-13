c1e9b97a0d0c4fc5005a6edcf2dc8509dbbf1698
Ryusen9 <asratul02@gmail.com>
2025-01-13 13:39:54 +0600

    Hero section integration and all necessary package installation

---
+++ src/Components/Hero.jsx
@@ -0,0 +1,12 @@
+
+const Hero = () => {
+  return (
+    <div className="min-h-screen w-full flex items-center justify-center overflow-x-hidden">
+        <div>
+            
+        </div>
+    </div>
+  )
+}
+
+export default Hero
---
+++ src/Layouts/Homelayout.jsx
@@ -0,0 +1,11 @@
+import Hero from "../Components/Hero"
+
+const HomeLayout = () => {
+  return (
+    <div>
+        <Hero/>
+    </div>
+  )
+}
+
+export default HomeLayout
