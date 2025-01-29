ec6cf15c850b3185112e78e4ddc86c78dcc4b1f5
Ryusen9 <asratul02@gmail.com>
2025-01-29 11:25:21 +0600

    Slider integration done

--- src/Components/Slider.jsx
+++ src/Components/Slider.jsx
@@ -7,7 +7,6 @@ import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 import "swiper/css/scrollbar";
-import { Fade } from "react-awesome-reveal";
 
 const Slider = () => {
   const slides = [
